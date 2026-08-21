5400570193b7a7bf967f0562342446eb773db0a0
amirreza valadkhani <amirreza.valadkhani@gmail.com>
2026-08-21 22:27:49 +0330

    feat: add prisma user repository

---
+++ src/modules/users/infrastructure/prisma-user-repository.ts
@@ -0,0 +1,29 @@
+import type { UserRepository } from '../application/repositories/user-repository.js';
+import type { User } from '../domain/user.js';
+
+import { prisma } from '../../../infrastructure/database/prisma/prisma-client.js';
+
+export class PrismaUserRepository implements UserRepository {
+  async create(user: User): Promise<User> {
+    return prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.email,
+        passwordHash: user.passwordHash,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        isActive: user.isActive,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+    });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return prisma.user.findUnique({ where: { email } });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return prisma.user.findUnique({ where: { id } });
+  }
+}
