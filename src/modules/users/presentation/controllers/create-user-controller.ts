abe48950b7358155ea28b089f2a936eefd6f62de
amirreza valadkhani <amirreza.valadkhani@gmail.com>
2026-08-23 21:54:32 +0330

    fix: hide sensitive user data from api response

--- src/modules/users/presentation/controllers/create-user-controller.ts
+++ src/modules/users/presentation/controllers/create-user-controller.ts
@@ -1,18 +1,19 @@
-import type { Request, Response, NextFunction } from 'express';
+import type {Request, Response, NextFunction} from 'express';
 
-import { createUserUseCase } from '../../../../app/container/user-container.js';
-import { sendSuccess } from '../../../../shared/http/api-response.js';
+import {createUserUseCase} from '../../../../app/container/user-container.js';
+import {sendSuccess} from '../../../../shared/http/api-response.js';
+import {toUserResponse} from '../dtos/user-response.js';
 
 export const createUserController = async (
-  request: Request,
-  response: Response,
-  next: NextFunction,
+    request: Request,
+    response: Response,
+    next: NextFunction,
 ): Promise<void> => {
-  try {
-    const user = await createUserUseCase.execute(request.body);
+    try {
+        const user = await createUserUseCase.execute(request.body);
 
-    sendSuccess(response, user, 201);
-  } catch (error: unknown) {
-    next(error);
-  }
+        sendSuccess(response, toUserResponse(user), 201);
+    } catch (error: unknown) {
+        next(error);
+    }
 };
