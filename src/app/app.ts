8069a420f899d3d5815784c5d8723493691484da
Amirreza Valadkhani <amirreza.valad@gmail.com>
2026-08-27 12:44:33 +0330

    feat: add cors package

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,12 @@ import { errorHandler } from './errors/error-handler.js';
 import { apiRouter } from './routes/index.js';
 import { requestIdMiddleware } from './middleware/request-id.js';
 import { requestLoggerMiddleware } from './middleware/request-logger.js';
+import cors from 'cors';
 
 export const createApp = (): Express => {
   const app = express();
+
+  app.use(cors());
 
   app.use(express.json());
 
