4d81c7c8bec7bdf0e0780e9ea6e63fa1c6e3427b
amirreza valadkhani <amirreza.valadkhani@gmail.com>
2026-08-28 14:21:49 +0330

    feat: add login http endpoint

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,22 +1,25 @@
-import { Router } from 'express';
+import {Router} from 'express';
 
-import { prisma } from '../../infrastructure/database/prisma/prisma-client.js';
-import { sendSuccess } from '../../shared/http/api-response.js';
-import { userRouter } from '../../modules/users/presentation/user-router.js';
+import {prisma} from '../../infrastructure/database/prisma/prisma-client.js';
+import {sendSuccess} from '../../shared/http/api-response.js';
+import {userRouter} from '../../modules/users/presentation/user-router.js';
+import {authRouter} from "../../modules/auth/presentation/auth-router.js";
 
 export const apiRouter: Router = Router();
 
 apiRouter.get('/health', async (_request, response, next) => {
-  try {
-    await prisma.$queryRaw`SELECT 1`;
+    try {
+        await prisma.$queryRaw`SELECT 1`;
 
-    sendSuccess(response, {
-      status: 'ok',
-      database: 'connected',
-    });
-  } catch (error: unknown) {
-    next(error);
-  }
+        sendSuccess(response, {
+            status: 'ok',
+            database: 'connected',
+        });
+    } catch (error: unknown) {
+        next(error);
+    }
 });
 
 apiRouter.use('/users', userRouter);
+
+apiRouter.use('/auth', authRouter);
---
+++ src/modules/auth/presentation/auth-router.ts
@@ -0,0 +1,7 @@
+import {Router} from 'express';
+
+import {loginController} from "./controllers/login-controller.js";
+
+export const authRouter: Router = Router();
+
+authRouter.post('/login', loginController);
