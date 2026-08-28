d08398eec4ad72270e214a868042d1ec293a6a6d
amirreza valadkhani <amirreza.valadkhani@gmail.com>
2026-08-28 20:25:11 +0330

    feat: add current user endpoint

---
+++ src/modules/users/presentation/controllers/get-current-user-controller.ts
@@ -0,0 +1,18 @@
+import {NextFunction, Request, Response} from "express";
+import {getUserByIdUseCase} from "../../../../app/container/user-container.js";
+import {sendSuccess} from "../../../../shared/http/api-response.js";
+import {toUserResponse} from "../dtos/user-response.js";
+
+export const getCurrentUserController = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const user = await getUserByIdUseCase.execute(request.userId);
+
+        sendSuccess(response, toUserResponse(user));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
--- src/modules/users/presentation/user-router.ts
+++ src/modules/users/presentation/user-router.ts
@@ -1,11 +1,20 @@
-import { Router } from 'express';
+import {Router} from 'express';
 
-import { validateRequest } from '../../../app/middleware/validate-request.js';
-import { createUserController } from './controllers/create-user-controller.js';
-import { getUserByIdController } from './controllers/get-user-by-id-controller.js';
-import { createUserSchema } from './schemas/create-user-schema.js';
+import {authenticationMiddleware} from "../../../app/middleware/authentication.js";
+import {getCurrentUserController} from "./controllers/get-current-user-controller.js";
+import {createUserController} from './controllers/create-user-controller.js';
+import {getUserByIdController} from './controllers/get-user-by-id-controller.js';
+
+import {validateRequest} from '../../../app/middleware/validate-request.js';
+import {createUserSchema} from './schemas/create-user-schema.js';
 
 export const userRouter: Router = Router();
 
-userRouter.get('/:id', getUserByIdController);
 userRouter.post('/', validateRequest(createUserSchema), createUserController);
+userRouter.get(
+    '/me',
+    authenticationMiddleware,
+    getCurrentUserController,
+)
+
+userRouter.get('/:id', getUserByIdController);
